9d5109105a540d44b4a38a5d2bd819bc4c4bdc46
burhan <burhan14031@iiitd.ac.in>
2016-12-07 16:00:43 +0530

    Add threading to avoid zombies

--- mp3Extractor.rb
+++ mp3Extractor.rb
@@ -29,6 +29,9 @@ puts "Number of links extracted : #{result.size}"
 
 # Iterate over each link and download the mp3 using existing tools online
 
+
+
+
 result.each do |vid|
 	vid = vid[0]
 
@@ -38,6 +41,8 @@ result.each do |vid|
 
 	# Get response on hitting the data_link 
 	data = open(data_link).read
+	puts data_link
+
 
 	info = {}
 	song_title = ""
@@ -48,6 +53,7 @@ result.each do |vid|
 		
 		info = JSON.parse(data)
 		song_title = info['title']
+		puts "JSON successful"
 
 	rescue Exception => e
 		puts e
@@ -56,21 +62,46 @@ result.each do |vid|
 		# Generate a random title
 		song_title = "song" + Random.rand(0..1000000).to_s
 
-
-		# Get main web page and extract DL link
-		mdp = open("http://www.youtubeinmp3.com/download/?video=https://www.youtube.com/watch?v=#{vid}").read
-		mdl = mdp.split('id="download')[1].split('href')[1].split('"')[1]
-		mdl = "http://www.youtubeinmp3.com" + mdl
-		
-		dl_link = mdl
+		begin
+			# Get main web page and extract DL link
+			mdp = open("http://www.youtubeinmp3.com/download/?video=https://www.youtube.com/watch?v=#{vid}").read
+			mdl = mdp.split('id="download')[1].split('href')[1].split('"')[1]
+			mdl = "http://www.youtubeinmp3.com" + mdl
+			
+			dl_link = mdl
+		rescue Exception => e
+			puts "Failed to download song. Trying next one."
+			next			
+		end
 	ensure
 	end
 
 
 	# Try downloading the current mp3
 	begin
-		`wget -O '#{song_title}.mp3' #{dl_link}`
-	rescue Exception => e
+		Thread.new do
+			`wget -nc --tries=5 -O '#{song_title}.mp3' #{dl_link}`
+		end.join
+
+		puts "#############################################################################"
+		# puts "STAT:#{thr.status}"
+		puts '#############################################################################'
+		# if (rv != 0)
+		# 	break
+		# end
+
+		Signal.trap("STOP") do
+			puts "ENDING PROCESS"
+			break
+		end
+		
+		Signal.trap("INT") do
+			puts "ENDING PROCESS"
+			break
+		end
+		
+
+		rescue Exception => e
 		puts e
 		puts "Failed to download #{vid}"
 	end
